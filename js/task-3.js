f8e0219254e4179414d07622324453faa469b568
ZatsepaTanya <Tanyamath@i.ua>
2020-12-03 03:08:14 +0200

    test-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,31 @@
-// Напиши фукцнию findLongestWord(string), которая принимает параметром 
-// произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+const findLongestWord = function(string){
+let words = string.split(" ");
+// console.log(words);
+let longestWord = words[0];
+// let sizeWord = longestWord.length;
+for (let i=1; i<words.length; i+=1){
+    // longestWord = words[i];
+    let sizeWord = longestWord.length;
+    if( sizeWord<words[i].length){
+        longestWord = words;
+        // return longestWord;
+    }
+    // return longestWord;
 
-// const findLongestWord = function(string) {
-//   // твой код
-// };
+    // console.log(longestWord);
+    console.log(sizeWord);
+   
+    
+}
+// console.log(longestWord);
+    // console.log(sizeWord);
+   
+// return longestWord;
+}
+// console.log(longestWord);
+// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+findLongestWord('The quick brown fox jumped over the lazy dog');
 
 // /*
 //  * Вызовы функции для проверки работоспособности твоей реализации.
