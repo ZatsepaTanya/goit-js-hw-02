dcc44f30c2e33c02bad21ec44fc95db8582a09f1
ZatsepaTanya <Tanyamath@i.ua>
2020-12-04 00:08:36 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,11 +1,29 @@
+const checkForSpam = function(message){
+
+    const word1 = "spam";
+    const word2 = "sale";
+    let result = false;
+    for( const word of words){
+        console.log(word);
+        if(word1===word || word2===word){
+            result = true;
+            break;
+
+        }
+    }
+console.log(result);
+}
+// checkForSpam('Latest technology news');
+// checkForSpam('JavaScript weekly newsletter');
+checkForSpam('Get best sale offers now!');
+checkForSpam('[SPAM] How to earn fast money?');
+
+
 // Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
 //  Функция проверяет ее на содержание слов spam и sale. Если нашли зарещенное слово 
 //  то функция возвращает true, если запрещенных слов нет функция возвращает false. 
 //  Слова в строке могут быть в произвольном регистре.
 
-// const checkForSpam = function(message) {
-//   // твой код
-// };
 
 // /*
 //  * Вызовы функции для проверки работоспособности твоей реализации.
