3c6676a36ca262cb9f3c149c96d2c3e2710d756a
ZatsepaTanya <Tanyamath@i.ua>
2020-12-03 20:31:07 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,37 +1,15 @@
 const findLongestWord = function(string){
 let words = string.split(" ");
-// console.log(words);
 let longestWord = words[0];
-// let sizeWord = longestWord.length;
 for (let i=1; i<words.length; i+=1){
-    // longestWord = words[i];
-    let sizeWord = longestWord.length;
-    if( sizeWord<words[i].length){
-        longestWord = words;
-        // return longestWord;
-    }
-    // return longestWord;
-
-    // console.log(longestWord);
-    console.log(sizeWord);
-   
-    
+     if( longestWord.length<words[i].length){
+        longestWord = words[i];
+    }   
 }
-// console.log(longestWord);
-    // console.log(sizeWord);
-   
-// return longestWord;
+console.log(longestWord); 
+return longestWord;
 }
-// console.log(longestWord);
-// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
 
 findLongestWord('The quick brown fox jumped over the lazy dog');
-
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
-
-// console.log(findLongestWord('Google do a roll')); // 'Google'
-
-// console.log(findLongestWord('May the force be with you')); // 'force'
+findLongestWord('Google do a roll');
+findLongestWord('May the force be with you');
